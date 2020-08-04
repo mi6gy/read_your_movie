5bae419abad6ef8882c352292f5001c21666d72b
Lane Martin <lane.martin49@gmail.com>
2020-08-04 13:42:57 -0400

    added link for materliaze

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,12 @@ $(document).ready(function () {
 
     event.preventDefault()
 
-    // var movie = $("#contentInput").val();
+    var movie = $("#contentInput").val();
     var book = $("#contentInput").val();
 
 
-    // var queryMoviesURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=101156dc";
-    var queryBooksURL = "http://openlibrary.org/search.json?title=" + book;
+    var queryMoviesURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=101156dc";
+    var queryBooksURL = "https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=AIzaSyBfde9KgHwbNNBEW_J011ZGwrHD0G0O1go"
 
 
 
@@ -19,20 +19,19 @@ $(document).ready(function () {
       method: "GET"
     }).then(function (response) {
       console.log(response);
-      $("#contentView").html(response[0]);
+      $("#bookView").html(response.items[0].volumeInfo.authors);
 
-      var bookTitle = response.docs[0].title
-      console.log(bookTitle)
+ 
     });
 
-    // $.ajax({
-    //   url: queryMoviesURL,
-    //   method: "GET"
-    // }).then(function (response) {
-    //   console.log(response);
-    //   $("#contentView").text(response.Title);
+    $.ajax({
+      url: queryMoviesURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+      $("#contentView").text(response.Title);
 
-    // });
+    });
 
   });
 
