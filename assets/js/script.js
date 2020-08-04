37046226b2b69499d4926d72ea0d5956d225984c
hojin Mun <jin1281@gmail.com>
2020-08-04 14:01:00 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
-
   $("#findContent").on("click", function (event) {
-
     event.preventDefault()
-
-    function displayMovieInfo() {
-
-      var movie = $(this).attr("data-name");
-      var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-
-      $.ajax({
-        url: queryMoviesURL,
-        method: "GET"
-      }).then(function (response) {
-
-        $("#contentView").text(JSON.stringify(response));
-        console.log(response);
-      });
-    };
+    var movie = $("#contentInput").val();
+    var book = $("#contentInput").val();
+    var queryMoviesURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=101156dc";
+    var queryBooksURL = "https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=AIzaSyBfde9KgHwbNNBEW_J011ZGwrHD0G0O1go"
+    $.ajax({
+      url: queryBooksURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+      $("#bookView").html(response.items[0].volumeInfo.authors);
+    });
+    $.ajax({
+      url: queryMoviesURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+      $("#contentView").text(response.Title);
+    });
   });
 });
