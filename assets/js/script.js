ae61fe42139bcb8f6dda8cf1b891e8025eeb2790
hojin Mun <jin1281@gmail.com>
2020-08-04 23:04:23 -0400

    updated ajax call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,40 @@
 $(document).ready(function () {
   $("#findContent").on("click", function (event) {
     event.preventDefault()
-    var movie = $("#contentInput").val();
-    var book = $("#contentInput").val();
-    var queryMoviesURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=101156dc";
-    var queryBooksURL = "https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=AIzaSyBfde9KgHwbNNBEW_J011ZGwrHD0G0O1go"
+        var book = $("#contentInput").val();
+       var queryBooksURL = "https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=AIzaSyBfde9KgHwbNNBEW_J011ZGwrHD0G0O1go"
+
+
+
+
     $.ajax({
       url: queryBooksURL,
       method: "GET"
-    }).then(function (response) {
-      console.log(response);
-      $("#bookView").html(response.items[0].volumeInfo.authors);
-    });
-    $.ajax({
-      url: queryMoviesURL,
-      method: "GET"
-    }).then(function (response) {
-      console.log(response);
-      $("#contentView").text(response.Title);
-    });
+    }).then(function (googleResponse) {
+      console.log(googleResponse);
+
+      // $("#bookView").html(googleResponse.items[0].volumeInfo.authors);
+      var queryMoviesURL = "https://www.omdbapi.com/?s=" + book  + "&apikey=101156dc";
+      $.ajax({
+        url: queryMoviesURL,
+        method: "GET"
+      }).then(function (movieResponse) {
+        console.log(movieResponse);
+        // $("#contentView").text(movieResponse.Title);
+        if(googleResponse.items.length > 0 && movieResponse.Response != "False"){
+          var bookImgURL = response.items[0].imageLinks.thumbnail;
+          $("#bookName").text(googleResponse.items[0].volumeInfo.title);
+          $("#card-title").html(googleResponse.items[0].volumeInfo.authors); 
+          $("#contentView").text(movieResponse.Title);
+          $("img").attr("src", bookImgURL);
+        }
+        else {
+          $("#bookView").html("<p>Movie not found</p>");
+        }
+      });
+    }).catch(function (err) {
+      console.log(err);
+    })
+
   });
 });
