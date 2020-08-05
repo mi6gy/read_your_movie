2f7e589199dfc4e823f3677d4fbaff5e703db295
Anthony Bergen <50747592+adbergen@users.noreply.github.com>
2020-08-04 23:06:04 -0400

    pulling master

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,7 @@ $(document).ready(function () {
 
 
     var queryMoviesURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=101156dc";
-<<<<<<< HEAD
-    var queryBooksURL = "http://openlibrary.org/search.json?title=" + book;
-=======
     var queryBooksURL = "https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=AIzaSyBfde9KgHwbNNBEW_J011ZGwrHD0G0O1go"
->>>>>>> ad3ffc0b9ad3cc91ae432624da2263eb47f701e1
 
 
 
@@ -25,7 +21,7 @@ $(document).ready(function () {
       console.log(response);
       $("#bookView").html(response.items[0].volumeInfo.authors);
 
- 
+
     });
 
     $.ajax({
